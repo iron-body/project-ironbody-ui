20ad06cb7f0fe23d641e0de7f0079029c05c3cf0
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-09-19 13:41:48 +0300

    login and logout logic
    login and logout logic with basic routes on heeroku.com

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,18 @@
 import { LogoStyled, PowerpulseIconSvg, VectorIconSvg } from './Logo.styled';
 import sprite from '../../../icons.svg';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/authSlice';
+import { useNavigate } from 'react-router-dom';
 
 export const Logo = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const navigate = useNavigate();
   return (
-    <LogoStyled to="/">
+    <LogoStyled
+      onClick={() => {
+        isLoggedIn ? navigate('/diary') : navigate('/');
+      }}
+    >
       <VectorIconSvg>
         <use href={`${sprite}#icon-logo36_13`} />
       </VectorIconSvg>
