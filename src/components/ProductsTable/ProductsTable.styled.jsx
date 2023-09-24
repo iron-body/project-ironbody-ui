74d13810f2eed6f651a2fbc36e55c99adea9232e
Anton K <akarpenko83@gmail.com>
2023-09-24 13:07:23 +0300

    media styles done

--- src/components/ProductsTable/ProductsTable.styled.jsx
+++ src/components/ProductsTable/ProductsTable.styled.jsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { device } from '../../Constants';
 
 export const TableContainer = styled.div`
-  max-height: 165px;
+  height: 100vh;
   overflow-y: auto;
   ::-webkit-scrollbar-track {
     background-color: transparent;
@@ -17,30 +17,52 @@ export const TableContainer = styled.div`
   ::-webkit-scrollbar-corner {
     background-color: #f1f1f1;
   }
-  @media ${device.desktop} {
+  @media ${device.tablet} {
+    max-height: 165px;
   }
 `;
 
 export const StyledHeadingTable = styled.div`
-  display: block;
+  display: flex;
+  justify-content: space-between;
   padding-left: 10px;
   color: rgba(239, 137, 100, 1);
   font-size: 12px;
   @media ${device.tablet} {
-    display: flex;
+    justify-content: flex-start;
   }
 `;
 export const StyledTitle = styled.p`
-  margin-right: 315px;
+  @media ${device.tablet} {
+    margin-right: 280px;
+  }
+  @media ${device.desktop} {
+    margin-right: 315px;
+  }
 `;
 export const StyledCategory = styled.p`
-  margin-right: 60px;
+  @media ${device.tablet} {
+    margin-right: 45px;
+  }
+  @media ${device.desktop} {
+    margin-right: 60px;
+  }
 `;
 export const StyledCalories = styled.p`
-  margin-right: 40px;
+  @media ${device.tablet} {
+    margin-right: 35px;
+  }
+  @media ${device.desktop} {
+    margin-right: 45px;
+  }
 `;
 export const StyledWeight = styled.p`
-  margin-right: 50px;
+  @media ${device.tablet} {
+    margin-right: 40px;
+  }
+  @media ${device.desktop} {
+    margin-right: 50px;
+  }
 `;
 
 export const StyledTable = styled.table`
@@ -51,9 +73,12 @@ export const StyledTable = styled.table`
 `;
 
 export const StyledTableRaw = styled.tr`
-  /* height: 40px; */
+  /* display: block; */
 `;
 export const StyledTableCell = styled.td`
+  display: inline-block;
+  margin-bottom: 8px;
+  margin-right: 10px;
   border: 1px solid gray;
   &:last-child {
     border: none;
@@ -61,6 +86,23 @@ export const StyledTableCell = styled.td`
   border-radius: 12px;
   font-size: 16px;
   padding: 12px 0px 12px 8px;
+  @media ${device.tablet} {
+    display: table-cell;
+    margin-bottom: 0px;
+  }
+`;
+
+export const StyledCell100 = styled.div`
+  width: 297px;
+  @media ${device.tablet} {
+    width: 100%;
+  }
+`;
+export const StyledCell30 = styled.div`
+  width: 81px;
+  @media ${device.tablet} {
+    width: 100%;
+  }
 `;
 export const StyledRec = styled.p`
   /* width: 25px; */
