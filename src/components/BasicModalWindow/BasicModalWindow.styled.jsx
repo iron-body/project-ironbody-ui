879fcee742119789e8d30234d654c66fed12ea19
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-09-25 23:01:49 +0300

    fix modal after Alina

--- src/components/BasicModalWindow/BasicModalWindow.styled.jsx
+++ src/components/BasicModalWindow/BasicModalWindow.styled.jsx
@@ -1,44 +1,22 @@
 import styled from 'styled-components';
 import { device } from '../../Constants';
-// export const ModalContainer = styled.div`
-//   height: 100vh;
-//   width: 100vw;
-//   background-color: rgba(0, 0, 0, 0.4);
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   opacity: 0;
-//   pointer-events: none;
-//   transition: 0.5s;
-
-//   &.active {
-//     opacity: 1;
-//     pointer-events: all;
-//   }
-// `;
-
 export const ModalContainer = styled.div`
-  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.4);
+  position: fixed;
   top: 0;
   left: 0;
   display: flex;
-  flex-direction: column;
-  height: 100vh;
-  width: 200px;
-  padding: 19px 20px 21px 20px;
-  background-color: #e6533c;
-  z-index: 9;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  pointer-events: none;
+  transition: 0.5s;
 
-  @media ${device.tablet} {
-    width: 350px;
-    padding: 26px 32px 32px 32px;
-  }
-
-  @media ${device.desktop} {
-    display: none;
+  &.active {
+    opacity: 1;
+    pointer-events: all;
   }
 `;
 
