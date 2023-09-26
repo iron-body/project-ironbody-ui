74119b8727fee0ddebbed75d3692b3ff69016705
IhorKiy <kiyanichenko@ukr.net>
2023-09-26 13:06:46 +0300

    26-1

--- src/components/ExercisesCategories/ExercisesCategories.styled.jsx
+++ src/components/ExercisesCategories/ExercisesCategories.styled.jsx
@@ -1,23 +1,36 @@
 import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { device } from '../../Constants';
 
 export const Container = styled.div`
   display: flex;
+  gap:8px;
   margin-top: 20px;
-  /* margin-bottom: 20 px; */
   color: #efede8;
   @media ${device.tablet} {
     margin: 0;
   }
 `;
 
-export const StyledLink = styled(Link)`
+export const StyledLink = styled(NavLink)`
   display: flex;
   align-items: center;
   flex-direction: column;
 
   color: rgba(239, 237, 232, 0.4);
+  &.active {
+    // transition-property: color;
+    // transition-duration: var(--transition-duration);
+    // transition-timing-function: var(--cubic-bezier);
+    color: #efede8;
+    &:after {
+      content: '';
+      margin-top: 4px;
+      width: 100%;
+      height: 3px;
+      background-color: #ef8964;
+      border-radius: 2px;
+  }
 
   @media screen and (${device.mobile}) {
     margin-right: 28px;
@@ -27,26 +40,8 @@ export const StyledLink = styled(Link)`
   @media screen and (${device.tablet}) {
     height: 32px;
     margin-right: 0px;
-    margin-top: 6px;
-    margin-left: 20px;
+    // margin-top: 6px;
+    // margin-left: 20px;
   }
 
-  &:hover,
-  &:focus {
-    transition-property: color;
-    transition-duration: var(--transition-duration);
-    transition-timing-function: var(--cubic-bezier);
-    color: #efede8;
-    &:after {
-      content: '';
-      margin-top: 4px;
-      width: 100%;
-      height: 3px;
-      background-color: #ef8964;
-      border-radius: 2px;
-      @media ${device.tablet} {
-        margin-top: 10px;
-      }
-    }
-  }
 `;
